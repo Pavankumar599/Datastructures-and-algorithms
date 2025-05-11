9008d4ef3b57a77d9823dfbb703842fd2c24413c
Pavankumar599 <medarametlapavankumar18@gmail.com>
2025-05-11 19:30:50 +0530

    updated primes

--- Basic maths for DSA/java/CountPrimes.java
+++ Basic maths for DSA/java/CountPrimes.java
@@ -3,12 +3,11 @@
  * problem link: https://leetcode.com/problems/count-primes/
  */
 import java.util.Scanner;
-import java.util.*;
  public class CountPrimes{
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n Value");
-        int n = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter n Value");
+            int n = sc.nextInt();
         
         boolean[] sieve = new boolean[n];
         for(int i = 2; i<n; i++){
@@ -25,7 +24,8 @@ import java.util.*;
                 }
             }
         }
-        System.out.println("prime numbers below "+ n + " are "+ count);
+            System.out.println("prime numbers below "+ n + " are "+ count);
+        }
 
     }
 
